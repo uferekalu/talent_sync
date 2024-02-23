6bd869b6e4ffa8442712f9aba9eeaf7211fb0a7e
Kalu Ufere <dekalusha@gmail.com>
2024-02-23 19:29:06 +0100

    error handling

--- church_app/src/components/register/RegisterModal.jsx
+++ church_app/src/components/register/RegisterModal.jsx
@@ -22,56 +22,103 @@ function RegisterModal({ createRegister, setCreateRegister }) {
     city: '',
     state: '',
     country: '',
-    medium: ''
+    medium: '',
   })
   const [successMsg, setSuccessMsg] = useState('')
-  const [errorMsg, setErrorMsg] = useState('')
+  const [errorMsg, setErrorMsg] = useState({})
+  const [serverError, setServerError] = useState('')
   const [registrationSuccess, setRegistrationSuccess] = useState(false)
 
   const handleInputChange = (event) => {
     const { name, value } = event.target
+
+    // Update the userData state
     setUserData({ ...userData, [name]: value })
+
+    if (value.length > 1) {
+      setErrorMsg((prevErrors) => ({
+        ...prevErrors,
+        [name]: '',
+      }))
+    }
   }
 
   const handleRegister = async () => {
-    try {
-      const response = await fetch('https://discipleship.onrender.com/api', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(userData),
-      })
-      if (!response.ok) {
-        const errorMessage = await response.json()
-        if (response.status === 400) {
-          setErrorMsg(errorMessage.message)
+    const newErrors = {}
+
+    if (userData.firstname.trim() === '') {
+      newErrors.firstname = 'First name cannot be empty'
+    }
+    if (userData.surname.trim() === '') {
+      newErrors.surname = 'Surname cannot be empty'
+    }
+    if (userData.email.trim() === '') {
+      newErrors.email = 'Email cannot be empty'
+    }
+    if (userData.phone.trim() === '') {
+      newErrors.phone = 'Phone cannot be empty'
+    }
+    if (userData.kit.trim() === '') {
+      newErrors.kit = 'Kit cannot be empty'
+    }
+    if (userData.address.trim() === '') {
+      newErrors.address = 'Address cannot be empty'
+    }
+    if (userData.city.trim() === '') {
+      newErrors.city = 'City cannot be empty'
+    }
+    if (userData.state.trim() === '') {
+      newErrors.state = 'State cannot be empty'
+    }
+    if (userData.country.trim() === '') {
+      newErrors.country = 'Country cannot be empty'
+    }
+    if (userData.medium.trim() === '') {
+      newErrors.medium = 'Medium cannot be empty'
+    }
+
+    if (Object.keys(newErrors).length > 0) {
+      setErrorMsg(newErrors)
+    } else {
+      try {
+        const response = await fetch('https://discipleship.onrender.com/api', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(userData),
+        })
+        if (!response.ok) {
+          const errorMessage = await response.json()
+          if (response.status === 400) {
+            setServerError(errorMessage.error)
+          }
+          return
         }
-        return
+        const data = await response.json()
+        if (data.message) {
+          setRegistrationSuccess(true)
+          setTimeout(() => {
+            setCreateRegister(false)
+            window.location.reload()
+          }, 3000)
+        }
+        setSuccessMsg(data.message)
+        setUserData({
+          firstname: '',
+          surname: '',
+          email: '',
+          phone: '',
+          kit: '',
+          address: '',
+          city: '',
+          state: '',
+          country: '',
+          medium: '',
+        })
+      } catch (error) {
+        setErrorMsg(error.message)
       }
-      const data = await response.json()
-      if (data.message) {
-        setRegistrationSuccess(true)
-        setTimeout(() => {
-          setCreateRegister(false)
-          window.location.reload()
-        }, 3000)
-      }
-      setSuccessMsg(data.message)
-      setUserData({
-        firstname: '',
-        surname: '',
-        email: '',
-        phone: '',
-        kit: '',
-        address: '',
-        city: '',
-        state: '',
-        country: '',
-        medium: ''
-      })
-    } catch (error) {
-      setErrorMsg(error.message)
     }
   }
 
@@ -98,12 +145,12 @@ function RegisterModal({ createRegister, setCreateRegister }) {
             onClose={() => setRegistrationSuccess(false)}
           />
         )}
-        {errorMsg && (
+        {serverError && (
           <DismissibleAlert
-            variant="success"
-            message={errorMsg}
+            variant="danger"
+            message={serverError}
             duration={3000}
-            onClose={() => setRegistrationSuccess(false)}
+            onClose={() => setServerError('')}
           />
         )}
         <motion.form className={classes.register__form}>
@@ -128,37 +175,50 @@ function RegisterModal({ createRegister, setCreateRegister }) {
             name="firstname"
             value={userData.firstname}
             onChange={handleInputChange}
-            className={classes.register__form__firstname}
+            className={`${classes.register__form__firstname} ${errorMsg.firstname && classes.register__form__errorBorder}`}
           />
+          {errorMsg.firstname && (
+            <span className={classes.error}>{errorMsg.firstname}</span>
+          )}
           <AnimatedInput
             type="text"
             placeholder="Surname"
             name="surname"
             value={userData.surname}
             onChange={handleInputChange}
-            className={classes.register__form__surname}
+            className={`${classes.register__form__surname} ${errorMsg.surname && classes.register__form__errorBorder}`}
           />
+          {errorMsg.surname && (
+            <span className={classes.error}>{errorMsg.surname}</span>
+          )}
           <AnimatedInput
             type="email"
             placeholder="Email"
             name="email"
             value={userData.email}
             onChange={handleInputChange}
-            className={classes.register__form__email}
+            className={`${classes.register__form__email} ${errorMsg.email && classes.register__form__errorBorder}`}
           />
+          {errorMsg.email && (
+            <span className={classes.error}>{errorMsg.email}</span>
+          )}
           <AnimatedInput
             type="text"
             placeholder="Phone"
             name="phone"
             value={userData.phone}
             onChange={handleInputChange}
-            className={classes.register__form__phone}
+            className={`${classes.register__form__phone} ${errorMsg.phone && classes.register__form__errorBorder}`}
           />
+          {errorMsg.phone && (
+            <span className={classes.error}>{errorMsg.phone}</span>
+          )}
           <motion.select
             initial={{ opacity: 0, x: -50 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.5 }}
-            className={classes.register__form__select}
+            // className={classes.register__form__select}
+            className={`${classes.register__form__select} ${errorMsg.kit && classes.register__form__errorBorder}`}
             onChange={handleInputChange}
             value={userData.kit}
             name="kit"
@@ -168,52 +228,84 @@ function RegisterModal({ createRegister, setCreateRegister }) {
             <option value="Survival Kit 2">Survival Kit 2</option>
             <option value="Arrival Kit">Arrival Kit</option>
           </motion.select>
+          {errorMsg.kit && (
+            <span className={classes.error}>{errorMsg.kit}</span>
+          )}
           <motion.select
             initial={{ opacity: 0, x: -50 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.5 }}
-            className={classes.register__form__select__medium}
+            // className={classes.register__form__select__medium}
+            className={`${classes.register__form__medium} ${errorMsg.medium && classes.register__form__errorBorder}`}
             onChange={handleInputChange}
             value={userData.medium}
             name="medium"
           >
             <option value="">Select means of doing the program</option>
             <option value="online">Online</option>
-            <option value="Physical (PCN Ikota Church)">{"Physical (PCN Ikota Church)"}</option>
-            
+            <option value="Physical (PCN Ikota Church)">
+              {'Physical (PCN Ikota Church)'}
+            </option>
           </motion.select>
+          {errorMsg.medium && (
+            <span className={classes.error}>{errorMsg.medium}</span>
+          )}
           <AnimatedInput
             type="text"
             placeholder="Address"
             name="address"
             value={userData.address}
             onChange={handleInputChange}
-            className={classes.register__form__address}
+            // className={classes.register__form__address}
+            className={`${classes.register__form__address} ${errorMsg.address && classes.register__form__errorBorder}`}
           />
+          {errorMsg.address && (
+            <span className={classes.error}>{errorMsg.address}</span>
+          )}
           <motion.div
             initial={{ opacity: 0, x: -50 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.5 }}
             className={classes.register__form__citystate__holder}
           >
-            <AnimatedInput
-              type="text"
-              placeholder="City"
-              name="city"
-              value={userData.city}
-              onChange={handleInputChange}
-              className={classes.register__form__citystate__holder__city}
-            />
-            <AnimatedInput
-              type="text"
-              placeholder="State"
-              name="state"
-              value={userData.state}
-              onChange={handleInputChange}
-              className={
-                classes.register__form__citystate__holder__state
-              }
-            />
+            <motion.div
+              initial={{ opacity: 0, x: -50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5 }}
+              className={classes.register__form__cityHolder}
+            >
+              <AnimatedInput
+                type="text"
+                placeholder="City"
+                name="city"
+                value={userData.city}
+                onChange={handleInputChange}
+                // className={classes.register__form__citystate__holder__city}
+                className={`${classes.register__form__citystate__holder__city} ${errorMsg.city && classes.register__form__errorBorder}`}
+              />
+              {errorMsg.city && (
+                <span className={classes.error__city}>{errorMsg.city}</span>
+              )}
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, x: -50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5 }}
+              className={classes.register__form__stateHolder}
+            >
+              <AnimatedInput
+                type="text"
+                placeholder="State"
+                name="state"
+                value={userData.state}
+                onChange={handleInputChange}
+                // className={classes.register__form__citystate__holder__state}
+                className={`${classes.register__form__citystate__holder__state} ${errorMsg.state && classes.register__form__errorBorder}`}
+              />
+              {errorMsg.state && (
+                <span className={classes.error__state}>{errorMsg.state}</span>
+              )}
+            </motion.div>
           </motion.div>
           <AnimatedInput
             type="text"
@@ -221,8 +313,12 @@ function RegisterModal({ createRegister, setCreateRegister }) {
             name="country"
             value={userData.country}
             onChange={handleInputChange}
-            className={classes.register__form__country}
+            // className={classes.register__form__country}
+            className={`${classes.register__form__country} ${errorMsg.state && classes.register__form__errorBorder}`}
           />
+          {errorMsg.country && (
+            <span className={classes.error}>{errorMsg.country}</span>
+          )}
           <AnimatedButton
             text="Submit"
             type="button"
